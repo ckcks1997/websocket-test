530750d1d86a71e0f9abe5803bc74f9f5899af4e
ckcks1997 <ckcks1997@gmail.com>
2023-10-10 23:54:54 +0900

    supplier 코드

---
+++ src/main/java/com/thehecklers/planefinder/PositionReporter.java
@@ -0,0 +1,28 @@
+package com.thehecklers.planefinder;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.function.Supplier;
+
+@AllArgsConstructor
+@Configuration
+public class PositionReporter {
+    private final PlaneFinderService pfService;
+
+    @Bean
+    Supplier<Iterable<Aircraft>> reportPositions() {
+        return () -> {
+            try {
+                Iterable<Aircraft> aircraft = pfService.getAircraft();
+                return aircraft;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return List.of();
+        };
+    }
+}
